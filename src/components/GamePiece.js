ad29ef9883e07aa8540dcb890002688702c7a694
Andrew Lombardi <andrew@mysticcoders.com>
2020-05-02 14:43:25 -0700

    naive? yes please

--- src/components/GamePiece.js
+++ src/components/GamePiece.js
@@ -1,22 +1,54 @@
 import React from 'react'
 
+export const WALL = {
+    NONE: null,
+    NORTH_WEST: 'NORTH_WEST',
+    SOUTH_WEST: 'SOUTH_WEST',
+    SOUTH_EAST: 'SOUTH_EAST',
+    NORTH: 'NORTH',
+    EAST: 'EAST',
+    SOUTH: 'SOUTH',
+    WEST: 'WEST'
+}
+
+export const ROBOT = {
+    NONE: null,
+    RED: 'RED',
+    BLUE: 'BLUE',
+    GREEN: 'GREEN',
+    YELLOW: 'YELLOW'
+}
+
+export const GOAL = {
+    NONE: null,
+    RED: 'RED',
+    BLUE: 'BLUE',
+    GREEN: 'GREEN',
+    YELLOW: 'YELLOW'
+}
 
 /**
  * Will contain the Retro Rockets gameboard
  */
-export const GamePiece = ({ type = 'BLANK', occupant }) => {
+export const GamePiece = ({ gridCell }) => {
 
+    const Robot = () => (
+        <div style={{ width: '25px', height: '25px', margin: '5px', background: gridCell.robot.toLowerCase(), borderRadius: '50%' }}>&nbsp;</div>
+    )
+    
+    const Goal = () => (
+        <div style={{ width: '25px', height: '25px', margin: '5px', background: gridCell.goal.toLowerCase(), borderRadius: '20%' }}>&nbsp;</div>
+    )
 
     return (
         <div style={{ width: '38px', height: '38px', border: '1px solid #000', backgroundColor: 'rgba(226, 206, 170, 1)'}}>
-            { occupant && 
-                <div style={{ width: '25px', height: '25px', margin: '5px', background: occupant, borderRadius: '50%' }}>&nbsp;</div>
+            { gridCell.robot && 
+                <Robot />
             }
             
         </div>
     )
 }
-
 
 
 export default GamePiece
--- src/containers/GameBoard.js
+++ src/containers/GameBoard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-import { GamePiece } from '../components/GamePiece'
+import { GamePiece, ROBOT } from '../components/GamePiece'
 
 import { Level } from 'rbx'
 /**
@@ -10,49 +10,36 @@ import { Level } from 'rbx'
 export const GameBoard = () => {
 
     
-    const rows = []
 
-    for(let i=0; i<16; i++) {
-        let occupant = undefined
-        let redOccupant = undefined
-        if(i === 4) {
-            occupant="blue"
-        }
+    const grid = []
 
-        if(i === 10) {
-            redOccupant="red"
-        }
-        rows.push(
-            <Level key={i} style={{margin: 0}}>
-                <Level.Item>
-                    <GamePiece occupant={occupant} />
-                    <GamePiece />
-                    <GamePiece />
-                    <GamePiece />
-                    <GamePiece />
-                    <GamePiece />
-                    <GamePiece />
-                    <GamePiece occupant={redOccupant} />
-                    <GamePiece />
-                    <GamePiece />
-                    <GamePiece />
-                    <GamePiece />
-                    <GamePiece />
-                    <GamePiece />
-                    <GamePiece />
-                    <GamePiece />
-                </Level.Item>
-            </Level>
-        )
+    for(let x = 0; x<16; x++) {
+        grid.push(new Array(16).fill({
+            walls: 0,
+            robot: null,
+            goal: null,
+        }))        
     }
+
+    console.dir(grid)
+
+    grid[4][8] = { ...grid[4][8], robot: ROBOT.BLUE}
+    grid[7][8] = { ...grid[7][8], robot: ROBOT.RED}
+    grid[14][10] = { ...grid[14][10], robot: ROBOT.YELLOW}
+    grid[2][2] = { ...grid[2][2], robot: ROBOT.GREEN}
+
     return (
-        <>
-        { rows.map(row => (
-            <>
-                {row}
-            </>
+        <div style={{ border: '5px solid #000', width: '618px'}}>
+        { grid.map((row, y) => (
+            <Level key={y} style={{margin: 0}}>
+                <Level.Item>
+                { row.map((cell, x) => (
+                    <GamePiece gridCell={grid[x][y]} />
+                ))}
+                </Level.Item>                
+            </Level>
         ))}
-        </>
+        </div>
     )
 }
 
