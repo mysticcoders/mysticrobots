1a7aa02f2e748383a1b6e18c1014af533631d4e7
Andrew Lombardi <andrew@mysticcoders.com>
2020-05-18 10:10:42 -0700

    start writing tests for the board

--- stack/common/test/board.test.js
+++ stack/common/test/board.test.js
@@ -1,10 +1,22 @@
 'use strict';
 
-const board = require('../lib/board');
-const assert = require('assert')
+const board = require('../lib/board')
 
 describe('board', () => {
     it('has a classic board', () => {
-        assert.ok(board.classic)
-    });
-});
+        expect(board.classic).toBeDefined()
+    })
+})
+
+describe('board.setupGoal', () => {
+    it('', () => {
+
+        // use same config so we get the same result and can test
+        const grid = {}
+        const config = board.setupBoard({ grid })
+
+        console.dir(config)
+        console.dir(grid)
+    })
+
+})
